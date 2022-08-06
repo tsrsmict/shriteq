50f6846d2a9580e9b7010d910fca216d850c4fde
Arjun Sharma <arjun.sharma07@outlook.com>
2022-08-06 16:21:48 +0530

    Add sentry

--- shriteq_website/settings.py
+++ shriteq_website/settings.py
@@ -37,6 +37,25 @@ ALLOWED_HOSTS = [
     'https://shriteq22.herokuapp.com/'
 ]
 
+import sentry_sdk
+from sentry_sdk.integrations.django import DjangoIntegration
+
+sentry_sdk.init(
+    dsn="https://df73c119bda044e1999b5e33075725c3@o879237.ingest.sentry.io/6628455",
+    integrations=[
+        DjangoIntegration(),
+    ],
+
+    # Set traces_sample_rate to 1.0 to capture 100%
+    # of transactions for performance monitoring.
+    # We recommend adjusting this value in production.
+    traces_sample_rate=1.0,
+
+    # If you wish to associate users to errors (assuming you are using
+    # django.contrib.auth) you may enable sending PII data.
+    send_default_pii=True
+)
+
 # Application definition
 
 INSTALLED_APPS = [
