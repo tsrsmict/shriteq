8434f0ea633ed2420f76bf59332f5310f8ce8db6
Arjun Sharma <arjun.sharma07@outlook.com>
2022-08-06 16:08:33 +0530

    Initial commit

--- shriteq_website/asgi.py
+++ shriteq_website/asgi.py
@@ -1,5 +1,5 @@
 """
-ASGI config for heroku_django_boilerplate project.
+ASGI config for shriteq_website project.
 
 It exposes the ASGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.asgi import get_asgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'heroku_django_boilerplate.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'shriteq_website.settings')
 
 application = get_asgi_application()
--- shriteq_website/wsgi.py
+++ shriteq_website/wsgi.py
@@ -1,5 +1,5 @@
 """
-WSGI config for heroku_django_boilerplate project.
+WSGI config for shriteq_website project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'heroku_django_boilerplate.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'shriteq_website.settings')
 
 application = get_wsgi_application()
