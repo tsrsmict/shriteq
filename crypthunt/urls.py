52ad87446522ed054a66ea71391002bcb39d78b0
Arjun Sharma <arjun.sharma07@outlook.com>
2022-08-17 01:54:53 +0530

    Fix CH urls

--- crypthunt/urls.py
+++ crypthunt/urls.py
@@ -3,7 +3,3 @@ from django.urls import path
 from django.views.generic import TemplateView
 
 from . import views
-
-urlpatterns = [
-    
-]
--- shriteq_website/urls.py
+++ shriteq_website/urls.py
@@ -20,6 +20,6 @@ from django.views.generic import TemplateView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('crypthunt/', 'crypthunt.site.urls'),
+    # path('crypthunt/', 'crypthunt.site.urls'),
     path('', TemplateView.as_view(template_name='index.html')),
 ]
