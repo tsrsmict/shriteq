6b3ce2d0f906f568f6d2ac0218893c6fa1653f80
Arjun Sharma <arjun.sharma07@outlook.com>
2022-08-20 16:41:24 +0530

    Doofus static

--- shriteq_website/urls.py
+++ shriteq_website/urls.py
@@ -16,6 +16,7 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 from django.views.generic import TemplateView
+from django.conf.urls.static import static
 
 
 urlpatterns = [
