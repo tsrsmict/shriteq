d9c2fb65444baf748711dcda2dae8273781c4c30
Arjun Sharma <arjun.sharma07@outlook.com>
2022-08-24 13:29:00 +0530

    Delete unnecessary files

--- events/views.py
+++ events/views.py
@@ -1,7 +1,21 @@
 from django.http import HttpResponse, HttpResponseNotFound
 from django.shortcuts import render
 
-from .slugs import EVENT_SLUGS
+EVENT_SLUGS = [
+    'hackathon',
+    'bot-trading',
+    'senior-quiz',
+    'middle-quiz',
+    'junior-quiz',
+    'crypt-hunt',
+    'competitive-gaming',
+    'photoshop-battles',
+    'ad-making',
+    'programming',
+    'drone-racing',
+    'web-design',
+    'pac-man',
+]
 
 def event(request, slug):
     if slug not in EVENT_SLUGS: return HttpResponseNotFound()
