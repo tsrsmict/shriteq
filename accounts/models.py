fdd2145a142d279ff630727f5276847acfc35481
Arjun Sharma <arjun.sharma07@outlook.com>
2022-09-11 17:43:41 +0530

    Migrate

--- accounts/models.py
+++ accounts/models.py
@@ -1,8 +1,6 @@
 from django.db import models
-
 from django.conf import settings
 
-# Create your models here.
 class School(models.Model):
     name = models.CharField(max_length=100)
     account = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
