1074044203e5522f3915af64fab8cb473aab5ee6
Arjun Sharma <arjun.sharma07@outlook.com>
2022-09-11 16:40:48 +0530

    Fix structure for pac man

--- pac_man/urls.py
+++ pac_man/urls.py
@@ -1,9 +1,23 @@
-from django.contrib import admin
+import datetime
+
 from django.urls import path
 from django.views.generic import TemplateView
 
+from django.conf import settings
 from . import views
 
-urlpatterns = [
-    path('', TemplateView.as_view(template_name='pac_man/waiting.html'), name='pac_man_waiting'),
-]
+urlpatterns = []
+
+
+now = datetime.datetime.now(tz=settings.TIME_ZONE_INFO)
+if settings.DEBUG == True or now >= settings.OPEN_EVENT_START_TIME:
+    urlpatterns += [
+        path('', views.index, name='pac_man_index'),
+        path('rules', TemplateView.as_view(template_name='pac_man/rules.html'), name='pac_man_rules'),
+        path('leaderboard', views.leaderboard, name='pac_man_leaderboard'),
+        path('play', views.play, name='pac_man_play'),
+    ]
+else: 
+    urlpatterns += [
+        path('', TemplateView.as_view(template_name='pac_man/waiting.html'), name='pac_man_waiting'),
+    ]
--- pac_man/views.py
+++ pac_man/views.py
@@ -1,3 +1,13 @@
 from django.shortcuts import render
 
 # Create your views here.
+def index(request):
+    return render(request, 'pac_man/index.html')
+
+def leaderboard(request):
+    context = {}
+    return render(request,'pac_man/leaderboard.html', context=context)
+
+def play(request):
+    context = {}
+    return render(request, 'pac_man/play.html', context=context)
