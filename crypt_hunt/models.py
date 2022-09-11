e6c11a14860d579631a60c050e185404814eb235
Arjun Sharma <arjun.sharma07@outlook.com>
2022-09-11 17:42:06 +0530

    Remove

--- crypt_hunt/models.py
+++ crypt_hunt/models.py
@@ -1,3 +1,14 @@
 from django.db import models
+from accounts.models import School
+import uuid
 
 # Create your models here.
+class Question(models.Model):
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    
+    serial_number = models.SmallIntegerField()
+    question = models.CharField(max_length=100)
+    answer = models.CharField(max_length=100)
+
+    def __str__(self):
+        return f'{self.serial_number}: {self.question}'
