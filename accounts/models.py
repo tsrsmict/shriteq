147f612ab747d04da61d2b3ebadcbf88b456c82f
Arjun Sharma <arjun.sharma07@outlook.com>
2022-09-11 17:42:06 +0530

    Remove

--- accounts/models.py
+++ accounts/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
+from django.conf import settings
+
 # Create your models here.
+class School(models.Model):
+    name = models.CharField(max_length=100)
+    account = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+    
+    ch_level = models.SmallIntegerField()
+    date_modified = models.DateTimeField(auto_now=True)
