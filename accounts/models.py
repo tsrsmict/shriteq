14b4943ba27f02f01f1edf439572dbf0c04c84d5
Arjun Sharma <arjun.sharma07@outlook.com>
2022-09-12 16:37:03 +0530

    Fix

--- accounts/models.py
+++ accounts/models.py
@@ -6,7 +6,8 @@ class School(models.Model):
     display_name = models.CharField(max_length=100)
     account = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
     
-    # question = models.ForeignKey(Question, on_delete=models.CASCADE, default=Question.objects.first(), null=True)
+    # Setting the default question to the first question in the database.
+    question = models.ForeignKey(Question, on_delete=models.CASCADE, default=Question.objects.first(), null=True)
     date_modified = models.DateTimeField(auto_now=True)
 
     def __str__(self):
