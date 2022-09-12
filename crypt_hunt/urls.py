8ffeaf2b681a968ddb9b56d526f8ac4eaf276088
Arjun Sharma <arjun.sharma07@outlook.com>
2022-09-12 08:30:04 +0530

    Move over settings logic

--- crypt_hunt/urls.py
+++ crypt_hunt/urls.py
@@ -9,8 +9,7 @@ from . import views
 urlpatterns = []
 
 
-now = datetime.datetime.now(tz=settings.TIME_ZONE_INFO)
-if settings.DEBUG == True or now >= settings.OPEN_EVENT_START_TIME:
+if settings.OPEN_EVENTS_RUNNING:
     urlpatterns += [
         path('', views.index, name='crypt_hunt_index'),
         path('rules', TemplateView.as_view(template_name='crypt_hunt/rules.html'), name='crypt_hunt_rules'),
