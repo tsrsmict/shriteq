a6c87d8ca11b59af1db11b71ed7e96799d7180f3
Arjun Sharma <arjun.sharma07@outlook.com>
2022-09-12 09:13:11 +0530

    Add check for CH

--- crypt_hunt/urls.py
+++ crypt_hunt/urls.py
@@ -11,11 +11,11 @@ urlpatterns = []
 
 if settings.OPEN_EVENTS_RUNNING:
     urlpatterns += [
-        path('', views.index, name='crypt_hunt_index'),
+        path('', views.Index.as_view(), name='crypt_hunt_index'),
         path('rules', TemplateView.as_view(template_name='crypt_hunt/rules.html'), name='crypt_hunt_rules'),
-        path('leaderboard', views.leaderboard, name='crypt_hunt_leaderboard'),
+        path('leaderboard', views.Index.as_view(), name='crypt_hunt_leaderboard'),
         path('play', views.Play.as_view(), name='crypt_hunt_play'),
-        path('congrats', views.congrats, name='crypt_hunt_congrats'),
+        path('congrats', views.Congrats.as_view(), name='crypt_hunt_congrats'),
     ]
 else: 
     urlpatterns += [
