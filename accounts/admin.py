b88377f45b6e9b210620ae41c84c90d6d373f609
Arjun Sharma <arjun.sharma07@outlook.com>
2022-09-12 16:34:47 +0530

    FIXX

--- accounts/admin.py
+++ accounts/admin.py
@@ -3,4 +3,5 @@ from django.contrib import admin
 
 @admin.register(School)
 class SchoolAdmin(admin.ModelAdmin):
-    list_display = ('display_name', 'question', 'date_modified')
+    # list_display = ('display_name', 'question', 'date_modified')
+    list_display = ('display_name', 'date_modified')
--- accounts/models.py
+++ accounts/models.py
@@ -6,7 +6,7 @@ class School(models.Model):
     display_name = models.CharField(max_length=100)
     account = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
     
-    question = models.ForeignKey(Question, on_delete=models.CASCADE, default=Question.objects.first(), null=True)
+    # question = models.ForeignKey(Question, on_delete=models.CASCADE, default=Question.objects.first(), null=True)
     date_modified = models.DateTimeField(auto_now=True)
 
     def __str__(self):
