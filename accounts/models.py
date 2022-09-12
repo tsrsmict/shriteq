7fdec538b2d59d6c6bc7acc0802b7b17bda07e79
Arjun Sharma <arjun.sharma07@outlook.com>
2022-09-12 17:34:17 +0530

    Remove

--- accounts/models.py
+++ accounts/models.py
@@ -2,17 +2,17 @@ from django.db import models
 from django.conf import settings
 from crypt_hunt.models import Question
 
-# class School(models.Model):
-#     display_name = models.CharField(max_length=100)
-#     account = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE, default=None, null=True)
+class School(models.Model):
+    display_name = models.CharField(max_length=100)
+    # account = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE, default=None, null=True)
     
-#     # Setting the default question to the first question in the database.
-#     question_num = models.SmallIntegerField(default=1, null=True)
-#     date_modified = models.DateTimeField(auto_now=True)
+    # Setting the default question to the first question in the database.
+    question_num = models.SmallIntegerField(default=1, null=True)
+    date_modified = models.DateTimeField(auto_now=True)
 
-#     def __str__(self):
-#         return self.account.username
+    def __str__(self):
+        return self.account.username
 
-#     @property
-#     def question(self):
-#         return Question.objects.all()[self.question_num - 1]
+    @property
+    def question(self):
+        return Question.objects.all()[self.question_num - 1]
