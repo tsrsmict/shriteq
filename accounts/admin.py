4e479af1895cb71d7bb43108382c63faf4865d65
Arjun Sharma <arjun.sharma07@outlook.com>
2022-09-12 18:18:21 +0530

    Fix admin

--- accounts/admin.py
+++ accounts/admin.py
@@ -4,4 +4,5 @@ from django.contrib import admin
 @admin.register(School)
 class SchoolAdmin(admin.ModelAdmin):
     # list_display = ('display_name', 'question', 'date_modified')
-    list_display = ('display_name', 'date_modified')
+    list_display = ('display_name', 'question_num', 'date_modified')
+    ordering = ('-question_num', 'display_name',)
