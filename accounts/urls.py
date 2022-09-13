27d1588cfb15844782aef3b79ba3f3cd26922ec0
Arjun Sharma <arjun.sharma07@outlook.com>
2022-09-13 13:28:56 +0530

    Remove URLs

--- accounts/urls.py
+++ accounts/urls.py
@@ -6,7 +6,10 @@ from django.views.generic import TemplateView
 from django.conf import settings
 from . import views
 
-urlpatterns = [
-    path('login/', views.login, name='login'),
-    path('logout/', views.logout, name='logout'),
-]
+urlpatterns = []
+
+if settings.OPEN_EVENTS_RUNNING:
+    urlpatterns = [
+        path('login/', views.login, name='login'),
+        path('logout/', views.logout, name='logout'),
+    ]
