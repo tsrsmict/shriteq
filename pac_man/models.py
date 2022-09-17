af9f39c1509c8ff1e58a7a6bd43003d5625359d9
Arjun Sharma <arjun.sharma07@outlook.com>
2022-09-17 17:19:04 +0530

    Add model

--- pac_man/models.py
+++ pac_man/models.py
@@ -1,3 +1,18 @@
 from django.db import models
+from accounts.models import School
 
 # Create your models here.
+class PacManPlayer(models.Model):
+    user_id = models.CharField(max_length=100)
+    school = models.ForeignKey(School, on_delete=models.CASCADE)
+    
+    num_attempts = models.IntegerField(default=0, editable=False)
+    high_score = models.IntegerField()
+    last_played = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return f'{self.user_id} - {self.school} - {self.high_score}'
+    
+    def __save__(self, *args, **kwargs):
+        self.num_attempts += 1
+        super().__save__(*args, **kwargs)
