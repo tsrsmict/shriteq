06d5b5b40699639dda56bcaaf97c8ff6286f807f
Arjun Sharma <arjun@bam.money>
2022-09-19 21:26:38 +0530

    Add backend for pac man

--- pac_man/urls.py
+++ pac_man/urls.py
@@ -13,7 +13,7 @@ if settings.OPEN_EVENTS_RUNNING:
         path('', views.Index.as_view(), name='pac_man_index'),
         path('rules', TemplateView.as_view(template_name='pac_man/rules.html'), name='pac_man_rules'),
         path('leaderboard', views.Leaderboard.as_view(), name='pac_man_leaderboard'),
-        path('play', views.Play.as_view(), name='pac_man_play'),
+        path('play/', views.Play.as_view(), name='pac_man_play'),
     ]
 else: 
     urlpatterns += [
