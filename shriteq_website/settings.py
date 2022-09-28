595f1d00d4b1b2a553a7a857468990b1741f92b4
Arjun Sharma <arjun.sharma07@outlook.com>
2022-09-28 18:09:19 +0530

    Add RTF

--- shriteq_website/settings.py
+++ shriteq_website/settings.py
@@ -88,6 +88,8 @@ INSTALLED_APPS = [
 
     'django_browser_reload',
     'django_components',
+    'ckeditor',
+    'ckeditor_uploader',
 
     'events',
     'accounts',
@@ -212,6 +214,10 @@ STATIC_ROOT = BASE_DIR / "staticfiles"
 
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 WHITENOISE_MANIFEST_STRICT = False
+
+CKEDITOR_UPLOAD_PATH = "uploads/"
+MEDIA_ROOT = BASE_DIR / "media"
+MEDIA_URL = "media/"
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
