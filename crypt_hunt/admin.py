86f73b8e670f715a8decd9dd3ec9daa4c7851283
Arjun Sharma <arjun.sharma2187@gmail.com>
2022-10-01 16:59:02 +0530

    Add submissions

--- crypt_hunt/admin.py
+++ crypt_hunt/admin.py
@@ -1,6 +1,10 @@
 from django.contrib import admin
-from .models import Question
+from .models import Question, Submission
 
 @admin.register(Question)
 class QuestionAdmin(admin.ModelAdmin):
     list_display = ('serial_num', 'question', 'answer')
+
+@admin.register(Submission)
+class SubmissionAdmin(admin.ModelAdmin):
+    list_display = ('question_num', 'contents', 'status', 'time', 'school', 'user_id', 'ip_address')
