751d751a6ddbe64bdb5497198f0d34a00f2edfa6
Arjun Sharma <arjun.sharma2187@gmail.com>
2022-10-04 19:01:18 +0530

    Fix user generator system

--- accounts/generate_accounts.py
+++ accounts/generate_accounts.py
@@ -14,7 +14,6 @@ f.close()
 
 for school in schools:
     print(f'Creating school {school}')
-    account_data = {'username': school['username'], 'password': school['password']}
-    user = User.objects.create(**account_data)
+    user = User.objects.create_user(username = school['username'], password=school['password'])
     school_data = {'display_name': school['display_name'], 'account': user}
     School.objects.create(**school_data)
