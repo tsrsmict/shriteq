8fac8b65e5c30f57d9be59476376130269fdffd8
Arjun Sharma <arjun.sharma07@Outlook.com>
2022-10-05 13:05:36 +0530

    Fix?

--- pac_man/urls.py
+++ pac_man/urls.py
@@ -14,7 +14,7 @@ if settings.DEBUG == True or now >= settings.OPEN_EVENT_START_TIME:
         path('', views.Index.as_view(), name='pac_man_index'),
         path('rules', TemplateView.as_view(template_name='pac_man/rules.html'), name='pac_man_rules'),
         path('leaderboard', views.Leaderboard.as_view(), name='pac_man_leaderboard'),
-        path('play/', views.Play.as_view(), name='pac_man_play'),
+        path('play', views.Play.as_view(), name='pac_man_play'),
     ]
 else: 
     if settings.IS_IN_EVENT_WINDOW:
