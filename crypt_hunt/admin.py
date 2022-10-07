daa45837e53e3de4ce4a9f7bbd19974e1ccdeebd
ConstantNorthernStar <aybutnotgone@gmail.com>
2022-10-08 00:37:47 +0530

    Add sorting to admin view

--- crypt_hunt/admin.py
+++ crypt_hunt/admin.py
@@ -4,6 +4,7 @@ from .models import Question, Submission
 @admin.register(Question)
 class QuestionAdmin(admin.ModelAdmin):
     list_display = ('serial_num', 'question', 'answer')
+    ordering = ('serial_num',)
 
 @admin.register(Submission)
 class SubmissionAdmin(admin.ModelAdmin):
