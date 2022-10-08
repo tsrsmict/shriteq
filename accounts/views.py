e8080106c6d2bc27f9fbcdb1e4b9e5efdd43f986
ArjunS07 <arjun.sharma07@outlook.com>
2022-10-08 18:34:21 +0530

    Add redirect

--- accounts/views.py
+++ accounts/views.py
@@ -13,10 +13,10 @@ def login(request):
     if request.method == 'POST':
         username = request.POST['school-username']
         password = request.POST['school-password']
-        user = auth.authenticate(username=username, password=password)
         user_id = request.POST['user-id'].lower().strip()
         if not is_valid_username(user_id):
-            return redirect('index')
+            return redirect('login')
+        user = auth.authenticate(username=username, password=password)
         if user is not None:
             auth.login(request, user)
             request.session['user_id'] = user_id
