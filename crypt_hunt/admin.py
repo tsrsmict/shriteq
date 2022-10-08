c0a6f3d3fb665dcb491fed3ba6cb4f1728275c2f
ArjunS07 <arjun.sharma07@outlook.com>
2022-10-08 12:47:12 +0530

    Add search field

--- crypt_hunt/admin.py
+++ crypt_hunt/admin.py
@@ -9,3 +9,4 @@ class QuestionAdmin(admin.ModelAdmin):
 @admin.register(Submission)
 class SubmissionAdmin(admin.ModelAdmin):
     list_display = ('question_num', 'contents', 'status', 'time', 'school', 'user_id', 'ip_address')
+    search_fields = ('question_num', 'contents', 'status', 'time', 'school', 'user_id', 'ip_address')
