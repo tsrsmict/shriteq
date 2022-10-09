381b69786ef1c1aeef7975801fa1a8217e3cf96f
ArjunS07 <arjun.sharma07@outlook.com>
2022-10-09 09:24:46 +0530

    fix

--- events/urls.py
+++ events/urls.py
@@ -3,7 +3,7 @@ from django.urls import path
 from django.views.generic import TemplateView
 
 from django.conf import settings
-from . import views
+from . import views 
 
 urlpatterns = [
     path('', TemplateView.as_view(template_name='index.html'), name='index'),
