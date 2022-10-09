07e1d8da655dbf308524ea1aa578e131410c7f3d
ArjunS07 <arjun.sharma07@outlook.com>
2022-10-09 20:36:27 +0530

    Fix

--- pac_man/views.py
+++ pac_man/views.py
@@ -55,7 +55,9 @@ class Play(BaseOnlineEventView):
         data = request.POST
         print(data)
         received_score = int(data['score'])
-        game_score = (((received_score - 3000) / 436) + 100) / 384893489
+        game_score = (((received_score - 3_000) / 436) + 100) / 384_893_489
+        if game_score > 200_000:
+            game_score = 200_000
         print(f'{received_score=}{game_score=}')
         user_id = data['user-id']
 
